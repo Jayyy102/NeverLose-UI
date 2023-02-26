47e5f4e6a796ed6c3f4563c1033e3406b4d12c00
Jayyy102 <108832889+Jayyy102@users.noreply.github.com>
2023-02-26 12:05:43 -0500

    Update NeverLoseUI.lua

--- NeverLoseUI.lua
+++ NeverLoseUI.lua
@@ -85,3 +85,75 @@ Section:Keybind({
         print(defaultBind)
     end
 })
+
+local Section = Tab:Section({
+    text = "AutoClicker"
+})
+
+Section:Button({
+    text = "Button",
+    callback = function()
+        print("Clicked button")
+    end,
+})
+
+Section:Toggle({
+    text = "Toggle",
+    state = false, -- Default boolean
+    callback = function(boolean)
+        print("Toggle current: ",boolean)
+    end
+})
+
+Section:Slider({
+    text = "Slider",
+    min = 10,
+    max = 100,
+    -- [[Float = 0,]] Idk what it does
+    callback = function(number)
+        print(number)
+    end
+})
+
+Section:Dropdown({
+    text = "Dropdown",
+    list = {"Apple", "Banana","Coconut"},
+    default = "Apple",
+    callback = function(String)
+        print(String)
+    end
+})
+
+Section:Textbox({
+    text = "Textbox",
+    value = "Default",
+    callback = function(String)
+        print(String)
+    end
+})
+
+Section:Colorpicker({
+    text = "Colorpicker",
+    color = Color3.new(1,1,1),
+    callback = function(HSV)
+        print(HSV)
+    end
+})
+
+--[[
+    blacklisted keybind:
+        Return
+        Space
+        Tab
+        W,A,S,D,I,O
+        Unknown
+]]
+
+Section:Keybind({
+    text = "Keybind",
+    default = Enum.KeyCode.Z,
+    callback = function(defaultBind)
+        print("Triggered keybind")
+        print(defaultBind)
+    end
+})
