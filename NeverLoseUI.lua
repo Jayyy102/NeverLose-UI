f2497026d9fac7f4b5cd5054f577314dcf10e1ad
Jayyy102 <108832889+Jayyy102@users.noreply.github.com>
2024-04-07 14:14:56 -0400

    Update NeverLoseUI.lua

--- NeverLoseUI.lua
+++ NeverLoseUI.lua
@@ -16,64 +16,14 @@ local Tab = TabSection:Tab({
 --AimAssist
 local Section = Tab:Section({
     text = "Aimassist"
+    callback = function(number)
+		print("Toggle current: ",boolean)
+end
 })
 
 Section:Toggle({
     text = "Toggle",
     state = false, -- Default boolean
-    Function = function(callback)
-			if callback then
-				RunLoops:BindToRenderStep("AimAssist", function(dt)
-					vapeTargetInfo.Targets.AimAssist = nil
-					if ((not AimAssistClickAim.Enabled) or (tick() - bedwars.SwordController.lastSwing) < 0.4) then
-						local plr = EntityNearPosition(18)
-						if plr then
-							vapeTargetInfo.Targets.AimAssist = {
-								Humanoid = {
-									Health = (plr.Character:GetAttribute("Health") or plr.Humanoid.Health) + getShieldAttribute(plr.Character),
-									MaxHealth = plr.Character:GetAttribute("MaxHealth") or plr.Humanoid.MaxHealth
-								},
-								Player = plr.Player
-							}
-							if bedwarsStore.localHand.Type == "sword" then
-								if GuiLibrary.ObjectsThatCanBeSaved["Lobby CheckToggle"].Api.Enabled then
-									if bedwarsStore.matchState == 0 then return end
-								end
-								if AimAssistTargetFrame.Walls.Enabled then 
-									if not bedwars.SwordController:canSee({instance = plr.Character, player = plr.Player, getInstance = function() return plr.Character end}) then return end
-								end
-								gameCamera.CFrame = gameCamera.CFrame:lerp(CFrame.new(gameCamera.CFrame.p, plr.Character.HumanoidRootPart.Position), ((1 / AimSpeed.Value) + (AimAssistStrafe.Enabled and (inputService:IsKeyDown(Enum.KeyCode.A) or inputService:IsKeyDown(Enum.KeyCode.D)) and 0.01 or 0)))
-							end
-						end
-					end
-				end)
-			else
-				RunLoops:UnbindFromRenderStep("AimAssist")
-				vapeTargetInfo.Targets.AimAssist = nil
-			end
-		end,
-		HoverText = "Smoothly aims to closest valid target with sword"
-	})
-	AimAssistTargetFrame = AimAssist.CreateTargetWindow({Default3 = true})
-	AimAssistClickAim = AimAssist.CreateToggle({
-		Name = "Click Aim",
-		Function = function() end,
-		Default = true,
-		HoverText = "Only aim while mouse is down"
-	})
-	AimAssistStrafe = AimAssist.CreateToggle({
-		Name = "Strafe increase",
-		Function = function() end,
-		HoverText = "Increase speed while strafing away from target"
-	})
-	AimSpeed = AimAssist.CreateSlider({
-		Name = "Smoothness",
-		Min = 1,
-		Max = 100, 
-		Function = function(val) end,
-		Default = 50
-	})
-end)
 })
 
 Section:Slider({
